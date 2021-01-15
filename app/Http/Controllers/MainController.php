f64767836f516d425d0f525058073a5e97649910
Renat <uid570@gmail.com>
2021-01-15 15:01:42 +0600

    4 commit

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,19 +9,23 @@ class MainController extends Controller
 {
     public function index() {
 
-        // $telegram = Telegram::getWebHookUpdates();
-        // $chat_id = $telegram['message']['chat']['id'];
+        $telegram = Telegram::getWebHookUpdates();
+        if (!empty($telegram)) {
+            $chat_id = $telegram['message']['chat']['id'];
+    
+            Telegram::sendMessage([
+                'chat_id' => $chat_id,
+                'text' => 'Hello!'
+            ]);
+        } else {
+            die("Empty");
+        }
 
-        // Telegram::sendMessage([
-        //     'chat_id' => $chat_id,
-        //     'text' => 'Hello!'
-        // ]);
-
-        $response = Telegram::getMe();
-        $botId = $response->getId();
-        $firstName = $response->getFirstName();
-        $username = $response->getUsername();
-        dd($response);
+        // $response = Telegram::getMe();
+        // $botId = $response->getId();
+        // $firstName = $response->getFirstName();
+        // $username = $response->getUsername();
+        // dd($response);
 
         // $data = json_decode(file_get_contents('php://input'), true);
         // file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
