fa1eefc6f8f23f632d4ed4ab888fccffa6b4b877
Renat <uid570@gmail.com>
2021-01-15 13:52:00 +0600

    4 commit

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,15 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class MainController extends Controller
 {
     public function index() {
 
-        $data = json_decode(file_get_contents('php://input'), true);
-        file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+        $telegram = Telegram::getWebHookUpdates();
+        $chat_id = $telegram['message']['chat']['id'];
 
-        dd($data);
+        Telegram::sendMessage([
+            'chat_id' => $chat_id,
+            'text' => 'Hello!'
+        ]);
+
+        // $response = Telegram::getMe();
+        // $botId = $response->getId();
+        // $firstName = $response->getFirstName();
+        // $username = $response->getUsername();
+        // dd($response);
+
+        // $data = json_decode(file_get_contents('php://input'), true);
+        // file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+
+        // dd($data);
 
         return "Hello!";
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 Route::get('/', 'App\Http\Controllers\MainController@index');
+Route::post('/bot/getupdates', function() {
+    $updates = Telegram::getUpdates();
+    return (json_encode($updates));
+});
